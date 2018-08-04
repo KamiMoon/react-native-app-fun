b77168e26e763d414d0779104bd1dc3fcff12e6e
Eric Kizaki <erickizaki@gmail.com>
2018-08-04 15:58:49 -0500

    first commit

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { createStackNavigator } from "react-navigation";
+
 // import Bannanas from "./src/Bananas";
 // import LotsOfGreeting from "./src/LotsOfGreetings";
 // import BlinkApp from "./src/BlinkApp";
@@ -14,31 +16,44 @@ import { StyleSheet, Text, View } from "react-native";
 //import FlatListBasics from "./src/FlatListBasics";
 //import SectionListBasics from "./src/SectionListBasics";
 import FetchExample from "./src/FetchExample";
+import HomeScreen from "./src/HomeScreen";
 
-export default class App extends React.Component {
-  render() {
-    // return (
-    //   <View style={styles.container}>
-    //     <Text>Open up App.js to start working on your app!</Text>
-    //     <Text>Changes you make will automatically reload.</Text>
-    //     <Text>Shake your phone to open the developer menu.</Text>
-    //     <Text>Hello World</Text>
-    //     <Bannanas />
-    //     <LotsOfGreeting />
-    //   </View>
-    // );
-    //return <BlinkApp />;
-    //return <LotsOfStyles />;
-    //return <FixedDimensionsBasics />;
-    //return <FlexDimensionsBasics />;
-    //return <PizzaTranslator />;
-    //return <Touchables />;
-    //return <IScrolledDownAndWhatHappenedNextShockedMe />;
-    //return <FlatListBasics />;
-    //return <SectionListBasics />;
-    return <FetchExample />;
+// export default class App extends React.Component {
+//   render() {
+//     // return (
+//     //   <View style={styles.container}>
+//     //     <Text>Open up App.js to start working on your app!</Text>
+//     //     <Text>Changes you make will automatically reload.</Text>
+//     //     <Text>Shake your phone to open the developer menu.</Text>
+//     //     <Text>Hello World</Text>
+//     //     <Bannanas />
+//     //     <LotsOfGreeting />
+//     //   </View>
+//     // );
+//     //return <BlinkApp />;
+//     //return <LotsOfStyles />;
+//     //return <FixedDimensionsBasics />;
+//     //return <FlexDimensionsBasics />;
+//     //return <PizzaTranslator />;
+//     //return <Touchables />;
+//     //return <IScrolledDownAndWhatHappenedNextShockedMe />;
+//     //return <FlatListBasics />;
+//     //return <SectionListBasics />;
+//     //return <FetchExample />;
+//   }
+// }
+
+const App = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Profile: { screen: FetchExample }
+  },
+  {
+    initialRouteName: "Home"
   }
-}
+);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
